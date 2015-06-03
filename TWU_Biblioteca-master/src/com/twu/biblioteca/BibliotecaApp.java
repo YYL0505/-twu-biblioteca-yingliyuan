614edba97d2ade432f177b84e79becc0e2f8b476
YYL0505 <453205401@qq.com>
2015-06-03 23:06:56 +0800

    add options.

--- TWU_Biblioteca-master/src/com/twu/biblioteca/BibliotecaApp.java
+++ TWU_Biblioteca-master/src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,6 +1,9 @@
 package com.twu.biblioteca;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class BibliotecaApp {
     private ArrayList<Book> books = null;
@@ -18,17 +21,43 @@ public class BibliotecaApp {
 
         BibliotecaApp app = new BibliotecaApp();
 
-        app.showBooks();
+        app.shoeOptions();
+
+    }
+
+    private void shoeOptions() {
+        Scanner scanner = new Scanner(System.in);
+
+
+        boolean quit = false;
+
+        while (!quit) {
+            System.out.println("******************1.List all books*******************");
+            System.out.println("******************2.Quit*****************************");
+            System.out.println("All options are listed here. You can choose one by inputting te number(before option):");
+            int index = scanner.nextInt();
+            switch (index) {
+                case 1:
+                    showBooks();
+                    break;
+                case 2:
+                    quit = true;
+                    break;
+                default:
+                    System.out.println("Select a valid option!");
+                    break;
+            }
+        }
 
     }
 
     private void showBooks() {
         System.out.println("All the books are listed below:");
-        System.out.println("Name            " + "author      " + "Year of Publish");
+        System.out.println("Order   Name            " + "author      " + "Year of Publish");
 
         for (int i = 0; i < books.size(); i++) {
             Book book = books.get(i);
-            System.out.println(book.getName() + "   " + book.getAuthor() + "    " + book.getYearOfPublished());
+            System.out.println((i + 1) + "    " + book.getName() + "   " + book.getAuthor() + "    " + book.getYearOfPublished());
         }
     }
 }
