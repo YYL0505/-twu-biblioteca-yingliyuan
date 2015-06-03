ea34697f2573d2d399dd47a43a7772c20810d4db
YYL0505 <453205401@qq.com>
2015-06-03 20:21:38 +0800

    add Book class.

--- TWU_Biblioteca-master/src/com/twu/biblioteca/Book.java
+++ TWU_Biblioteca-master/src/com/twu/biblioteca/Book.java
@@ -4,4 +4,13 @@ package com.twu.biblioteca;
  * Created by YYL-DELL on 2015/6/3.
  */
 public class Book {
+    private String name = null;
+    private String author = null;
+    private String yearOfPublished = null;
+
+    public Book(String name, String author, String year) {
+        this.name = name;
+        this.author = author;
+        this.yearOfPublished = year;
+    }
 }
