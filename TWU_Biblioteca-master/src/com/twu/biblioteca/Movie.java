df10bf4058204b6adc2da3c6250c8c2f0c08ef9f
YYL0505 <453205401@qq.com>
2015-06-07 14:42:21 +0800

    add Movie class.

---
+++ TWU_Biblioteca-master/src/com/twu/biblioteca/Movie.java
@@ -0,0 +1,18 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by YYL-DELL on 2015/6/7.
+ */
+public class Movie {
+    private String name;
+    private String year;
+    private String director;
+    private String rate;
+
+    public Movie(String name, String year, String director, String rate) {
+        this.name = name;
+        this.year = year;
+        this.director = director;
+        this.rate = rate;
+    }
+}
