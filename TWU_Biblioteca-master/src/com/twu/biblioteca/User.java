fe40181ff9bf86da6066c6284069c3e31610ed86
YYL0505 <453205401@qq.com>
2015-06-07 15:21:20 +0800

    addUser class

--- TWU_Biblioteca-master/src/com/twu/biblioteca/User.java
+++ TWU_Biblioteca-master/src/com/twu/biblioteca/User.java
@@ -4,4 +4,13 @@ package com.twu.biblioteca;
  * Created by YYL-DELL on 2015/6/7.
  */
 public class User {
+    private String libraryNumber;
+    private String password;
+    private String userType;
+
+    public User(String libraryNumber, String password, String userType) {
+        this.libraryNumber = libraryNumber;
+        this.password = password;
+        this.userType = userType;
+    }
 }
